271bc7dbc5bc98764f2cfcda97eace2956eae504
Neka <nekoanchor@yahoo.fr>
2021-01-19 21:09:54 +0100

    add shaksp

---
+++ lib/shakespeare.rb
@@ -0,0 +1,5 @@
+dictionnary = ["the", "of", "and", "to", "a", "in", "for", "is", "on", "that", "by", "this", "with", "i", "you", "it", "not", "or", "be", "are"]dictionnary = ["the", "of", "and", "to", "a", "in", "for", "is", "on", "that", "by", "this", "with", "i", "you", "it", "not", "or", "be", "are"]
+
+def name
+
+end
